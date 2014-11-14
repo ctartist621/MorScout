90ab5e7f7aedf140aaa601303ad03118f2828e94
Farbod909 <rafezyfarbod@gmail.com>
2014-11-13 22:32:24 -0800

    Fixed login bug

--- webapp/Login.js
+++ webapp/Login.js
@@ -1,17 +1,17 @@
 function validateForm() {
-    		var x = document.forms["myForm"]["user"].value;
-    		if (x == null || x == "") {
-      			alert("username must contain something");
-			console.log("x is blank");
-      	 		return false;
-		}
-		console.log("x is not blank");
-		var y = document.forms["myForm"]["pass"].value;
-    		if (y == null || y == "") {
-      			alert("Password must contain something");
-			console.log("y is blank");
-      	 		return false;
-		}
-		console.log("y is not blank");
-		return true;
-    	}
+    var x = document.forms["myForm"]["username"].value;
+    if (x == null || x == "") {
+		alert("username must contain something");
+		console.log("x is blank");
+      	return false;
+	}
+	console.log("x is not blank");
+	var y = document.forms["myForm"]["password"].value;
+    if (y == null || y == "") {
+      	alert("Password must contain something");
+		console.log("y is blank");
+      	return false;
+	}
+	console.log("y is not blank");
+	return true;
+}
