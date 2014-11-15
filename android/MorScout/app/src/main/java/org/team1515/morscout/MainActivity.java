c22e37ec9d0fecd9dcf4fceaa46c5ec40f90452d
DaPasta <484902@gmail.com>
2014-11-15 11:17:09 -0800

    Added sync fragment

--- android/MorScout/app/src/main/java/org/team1515/morscout/MainActivity.java
+++ android/MorScout/app/src/main/java/org/team1515/morscout/MainActivity.java
@@ -13,13 +13,10 @@ import android.view.ViewGroup;
 
 
 public class MainActivity extends FragmentActivity
-        implements NavigationDrawerFragment.NavigationDrawerCallbacks, TeamsFragment.OnFragmentInteractionListener, MatchesFragment.OnFragmentInteractionListener {
+        implements NavigationDrawerFragment.NavigationDrawerCallbacks, TeamsFragment.OnFragmentInteractionListener, MatchesFragment.OnFragmentInteractionListener, SyncFragment.OnFragmentInteractionListener {
 
     //Fragment managing the behaviors, interactions and presentation of the navigation drawer.
     private NavigationDrawerFragment mNavigationDrawerFragment;
-
-    //Used to store the last screen title. For use in {@link #restoreActionBar()}.
-    private CharSequence mTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,9 +28,7 @@ public class MainActivity extends FragmentActivity
 
     getFragmentManager().findFragmentById(R.id.navigation_drawer);
 
-    mTitle= getTitle();
-
-    // Set up the drawer.
+        // Set up the drawer.
     mNavigationDrawerFragment.setUp(R.id.navigation_drawer, (DrawerLayout)findViewById(R.id.drawer_layout));
     }
 
@@ -46,21 +41,24 @@ public class MainActivity extends FragmentActivity
         switch (position) {
             case 0:
                 fragment = new MatchesFragment();
+                setTitle(R.string.title_fragment_matches);
                 break;
             case 1:
                 fragment = new TeamsFragment();
+                setTitle(R.string.title_fragment_teams);
                 break;
             case 2:
-                fragment = new MatchesFragment();
+                fragment = new SyncFragment();
+                setTitle(R.string.title_fragment_sync);
                 break;
             default:
                 fragment = new MatchesFragment();
+                setTitle(R.string.title_fragment_matches);
         }
 
         // update the main content by replacing fragments
         FragmentManager fragmentManager = getSupportFragmentManager();
         fragmentManager.beginTransaction()
-//                .replace(R.id.container, PlaceholderFragment.newInstance(position + 1))
                 .replace(R.id.container, fragment)
                 .commit();
     }
@@ -69,13 +67,10 @@ public class MainActivity extends FragmentActivity
     public void onSectionAttached(int number) {
         switch (number) {
             case 1:
-                mTitle = getString(R.string.navigation_frame1);
                 break;
             case 2:
-                mTitle = getString(R.string.navigation_frame2);
                 break;
             case 3:
-                mTitle = getString(R.string.navigation_frame3);
                 break;
         }
     }
@@ -85,45 +80,5 @@ public class MainActivity extends FragmentActivity
 
     }
 
-
-/**
- * A placeholder fragment containing a simple view.
- */
-public static class PlaceholderFragment extends Fragment {
-    /**
-     * The fragment argument representing the section number for this
-     * fragment.
-     */
-    private static final String ARG_SECTION_NUMBER = "section_number";
-
-    /**
-     * Returns a new instance of this fragment for the given section
-     * number.
-     */
-    public static PlaceholderFragment newInstance(int sectionNumber) {
-        PlaceholderFragment fragment = new PlaceholderFragment();
-        Bundle args = new Bundle();
-        args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    public PlaceholderFragment() {
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_matches, container, false);
-    }
-
-    @Override
-    public void onAttach(Activity activity) {
-        super.onAttach(activity);
-        ((MainActivity) activity).onSectionAttached(
-                getArguments().getInt(ARG_SECTION_NUMBER));
-    }
-
-}
 
 }
