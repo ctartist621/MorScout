70e2725c3e62bae81f340d8332adff74aacce477
DaPasta <484902@gmail.com>
2014-12-12 17:15:40 -0800

    Android application sends POST data to login to server.

--- android/MorScout/app/src/main/java/org/team1515/client/Sync.java
+++ android/MorScout/app/src/main/java/org/team1515/client/Sync.java
@@ -3,6 +3,15 @@ package org.team1515.client;
 import android.os.AsyncTask;
 import android.util.Log;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -10,49 +19,33 @@ import java.io.Reader;
 import java.net.HttpURLConnection;
 import java.net.ProtocolException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Sync extends AsyncTask<URL, Void, String>{
     @Override
     protected String doInBackground(URL... urls) {
-        InputStream inputStream = null;
-        // Only display the first 500 characters of the retrieved web page content.
-        int length = 500;
+        // Create a new HttpClient and Post Header
+        HttpClient httpclient = new DefaultHttpClient();
+        HttpPost httppost = new HttpPost(urls[0].toString());
 
         try {
-            URL url = urls[0];
+            // Add your data
+            List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
+            nameValuePairs.add(new BasicNameValuePair("username", "BenH"));
+            nameValuePairs.add(new BasicNameValuePair("password", "pigeon"));
+            httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
 
-            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-            urlConnection.setReadTimeout(10000 /* milliseconds */);
-            urlConnection.setConnectTimeout(5000 /* milliseconds */);
-            urlConnection.setRequestMethod("GET");
-            urlConnection.setDoInput(true);
+            // Execute HTTP Post Request
+            HttpResponse response = httpclient.execute(httppost);
+            return readIt(response.getEntity().getContent(), 500);
 
-            // Starts the query
-            urlConnection.connect();
-            int response = urlConnection.getResponseCode(); //generate http code
-            Log.d("Http Response", "The response inputStream: " + response);
-            inputStream = urlConnection.getInputStream();
-
-            // Convert the InputStream into a string
-            return readIt(inputStream, length);
-
-        } catch (ProtocolException e) {
+        } catch (ClientProtocolException e) {
             e.printStackTrace();
-            return "error";
         } catch (IOException e) {
             e.printStackTrace();
-            return "error";
-        } finally {
-            if (inputStream != null) {
-                try {
-                    // Makes sure that the InputStream inputStream closed after the app inputStream
-                    // finished using it.
-                    inputStream.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
         }
+        return "Error";
     }
 
     // Reads an InputStream and converts it to a String.
--- android/MorScout/app/src/main/java/org/team1515/morscout/SyncFragment.java
+++ android/MorScout/app/src/main/java/org/team1515/morscout/SyncFragment.java
@@ -11,9 +11,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
-import android.widget.TextView;
-
-import org.team1515.client.Sync;
 
 
 /**
