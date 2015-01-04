fa21c63f7ff41d6c7248aa78ef29d6b16995f799
Farbod909 <rafezyfarbod@gmail.com>
2015-01-04 12:37:48 -0800

    Added iOS app.

---
+++ iOS/www/js/login.js
@@ -0,0 +1,79 @@
+function getQS(obj) {
+	var arr = Object.keys(obj);
+	for(var i = 0; i < arr.length; i++) {
+		arr[i] = escape(arr[i]) + "=" + escape(obj[arr[i]]);
+	}
+	return arr.join("&");
+}
+function parseQS(str) {
+	var arr = str.split("&");
+	var obj = {};
+	for(var i = 0; i < arr.length; i++) {
+		var pair = arr[i].split("=");
+		obj[unescape(pair[0])] = unescape(pair[1] || "");
+	}
+	return obj;
+}
+function ajax(url, get, post, cb) {
+	var qs = getQS(get);
+	if(qs != "") {
+		url += "?" + qs;
+	}
+	var xhr = new XMLHttpRequest() || new XDomainRequest();
+	xhr.onload = function() {
+		if(cb) {
+			cb(parseQS(xhr.responseText));
+		}
+	};
+	xhr.open("POST", url, true);
+	xhr.send(getQS(post));
+}
+$(document).ready(function() {
+	sessionStorage.removeItem('user')	
+	sessionStorage.removeItem('token')
+	if(localStorage.ip == undefined){
+		localStorage.ip = localhost;
+	}
+});
+document.getElementById("login_form").onsubmit = function() {
+	if($('#check_box').is(':checked')){
+
+		var user = document.getElementById("username").value;
+		var pass = document.getElementById("password").value;
+		
+		ajax("http://" + localStorage.ip + ":8080/login", {}, {"user" : user, "pass" : pass}, function(result) {
+			if(result.code == 0) {
+				localStorage.user = result.user;
+				localStorage.token = result.token;
+				location = "index.html";
+			}
+			else if(result.code == 1) {
+				alert("invalid username and password");
+			}
+			else {
+				alert("oops");
+			}
+		});
+		
+	} else {
+		
+		var user = document.getElementById("username").value;
+		var pass = document.getElementById("password").value;
+		
+		ajax("http://" + localStorage.ip + ":8080/login", {}, {"user" : user, "pass" : pass}, function(result) {
+			if(result.code == 0) {
+				sessionStorage.user = result.user;
+				sessionStorage.token = result.token;
+				location = "index.html";
+			}
+			else if(result.code == 1) {
+				alert("invalid username and password");
+			}
+			else {
+				alert("oops");
+			}
+		});
+
+	}	
+	return false;
+};
---
+++ iOS/www/js/teamsTable.js
@@ -0,0 +1,79 @@
+$(document).ready(function() {
+	
+	// !!! VERY IMPORTANT !!! OPEN IN FIREFOX! CHROME DOES NOT ALLOW LOCAL AJAX
+	
+	$.getJSON('json/teamsJson.json', function(jsonfile){
+		
+		var table = document.createElement('table');
+		
+		var header_row = document.createElement('tr');
+		
+		var th1 = document.createElement('th');
+		var th2 = document.createElement('th');
+		
+		var th_text1 = document.createTextNode("#");
+		var th_text2 = document.createTextNode("Team");
+		
+		th1.appendChild(th_text1);
+		th2.appendChild(th_text2);
+		
+		header_row.appendChild(th1);
+		header_row.appendChild(th2);
+		
+		table.appendChild(header_row);
+		
+		for (match_number = 0 ; match_number < Object.keys(jsonfile).length  ; match_number++) {
+				
+			var real_match_number = Object.keys(jsonfile)[match_number];
+			var tr = document.createElement('tr');
+
+			var td1 = document.createElement('td');
+			var td2 = document.createElement('td');
+
+			var text1 = document.createTextNode(real_match_number);
+			var text2 = document.createTextNode(jsonfile[real_match_number].team);
+
+			td1.appendChild(text1);
+			td2.appendChild(text2);
+				
+			tr.appendChild(td1);
+			tr.appendChild(td2);
+
+			table.appendChild(tr);
+
+		};
+		
+		document.body.appendChild(table);
+		
+		$('#loading').hide();
+		$("tr:odd").addClass("odd_row");
+		$("tr:first").addClass("first_row");
+		$(table).addClass('teams_table');
+		$("tr:last").attr('id', 'scrollHere');
+		
+		$('tr').not(".first_row").mouseenter(function(){
+		    $(this).addClass('table_hover');
+		});
+		
+		$('tr').not(".first_row").mouseleave(function(){
+		    $(this).removeClass('table_hover');
+		});
+		
+		$('tr').not(".first_row").click( function(){
+			if(document.getElementById('team_report_area') == null){
+				var reportBox = document.createElement('textarea');
+				$(reportBox).addClass('report_area');
+				$(reportBox).attr('id', 'team_report_area');
+				$(reportBox).attr("placeholder", "Enter REPORT here...");
+				$(reportBox).hide();
+				document.body.appendChild(reportBox);
+				$(reportBox).fadeIn(500);
+				location = '#scrollHere';
+				$(reportBox).focus();
+			}else{
+				alert("You already have a form open!");
+			}
+		});
+
+	});
+});
